aa151ce0be3a841d12ece310f6f6a6464291ec5d
sftt <gavingao@zju.edu.cn>
2016-07-22 11:15:42 +0800

    Revert "修正delete缺失定义导致编译不通过问题"

--- _msvcrt.cpp
+++ _msvcrt.cpp
@@ -1,15 +1,19 @@
 #pragma once
 
+//#include "stdafx.h"
+
 #if NDEBUG&&_DLL
 #include <vcruntime_new.h>
-#include <corecrt_terminate.h>
+#include <Windows.h>
 
-#ifdef __NOTHROW_T_DEFINED
+#ifdef __cplusplus
+
 namespace std
 {
 	const nothrow_t nothrow;
 }
 #endif
+
 
 extern "C" void __fastcall _guard_check_icall(void*)
 {
